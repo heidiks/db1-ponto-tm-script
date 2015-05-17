61bf92f970f5107cda3f155e731187bdb9350c33
Heidi <heidiks@gmail.com>
2015-05-17 17:33:59 -0300

    Refactory

--- calculator.js
+++ calculator.js
@@ -1,25 +1,28 @@
-var manhaInicio = "08:17:24";
-var manhaFim = "12:03:39";
-var tardeInicio = "13:17:34";
-
-/*var manhaInicio = $('.tabExterna tr').last().children().eq(1).html();
+var manhaInicio = $('.tabExterna tr').last().children().eq(1).html();
 var manhaFim = $('.tabExterna tr').last().children().eq(2).html();
-var tardeInicio = $('.tabExterna tr').last().children().eq(3).html();*/
+var tardeInicio = $('.tabExterna tr').last().children().eq(3).html();
 
-if(manhaInicio == "" || manhaFim == "" || tardeInicio == "") 
+if(manhaInicio == "" || manhaFim == "" || tardeInicio == "") {
     console.log("Dados Insuficientes!");
+	return;
+} 
+
+var calculaDiferenca = function(horaA, horaB) {
+	var momentA = criaMoment(horaA);
+	var momentB = criaMoment(horaB);
+
+	return momentA.diff(momentB, 'second', true);
+}
+
+var criaMoment = function(hora) {
+	return moment(DIA_FICTICIO + hora);
+}
 
 var DIA_FICTICIO = "2011-01-01 ";
 var JORNADA_TOTAL_SEGUNDOS = 31680;
 
-var a = moment(DIA_FICTICIO + manhaInicio);
-var b = moment(DIA_FICTICIO + manhaFim);
+var periodoTrabalhadoManha = calculaDiferenca(manhaFim, manhaInicio);
 
-var periodoTrabalhadoManha = b.diff(a, 'second', true);
-
-//console.log(b.diff(a, 'second', true));
-//console.log(JORNADA_TOTAL_SEGUNDOS - periodoTrabalhadoManha);
-
-var horaSaida = moment(DIA_FICTICIO + tardeInicio).add(JORNADA_TOTAL_SEGUNDOS - periodoTrabalhadoManha, "second");
+var horaSaida = criaMoment(tardeInicio).add(JORNADA_TOTAL_SEGUNDOS - periodoTrabalhadoManha, "second");
 console.log("Hora de saída: " + horaSaida.subtract(10, 'minutes').format("HH:mm:ss"));
 console.log("Hora extra: " + horaSaida.add(20, 'minutes').format("HH:mm:ss"));

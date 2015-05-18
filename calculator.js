3414b5022acf6092e8042aedefb3d05438e0abf4
Heidi <heidiks@gmail.com>
2015-05-17 21:00:23 -0300

    Calculador

--- calculator.js
+++ calculator.js
@@ -24,5 +24,12 @@ var JORNADA_TOTAL_SEGUNDOS = 31680;
 var periodoTrabalhadoManha = calculaDiferenca(manhaFim, manhaInicio);
 
 var horaSaida = criaMoment(tardeInicio).add(JORNADA_TOTAL_SEGUNDOS - periodoTrabalhadoManha, "second");
-console.log("Hora de saída: " + horaSaida.subtract(10, 'minutes').format("HH:mm:ss"));
-console.log("Hora extra: " + horaSaida.add(20, 'minutes').format("HH:mm:ss"));
+//console.log("Hora de saída: " + horaSaida.subtract(10, 'minutes').format("HH:mm:ss"));
+//console.log("Hora extra: " + horaSaida.add(20, 'minutes').format("HH:mm:ss"));
+$("body").append (
+	'<div id="dvCalculator"><span class="btn btn-lg btn-primary glyphicon glyphicon-ok" title="Hora minima de  sa&iacute;da"> Sa&iacute;da<br>&nbsp;'
+	+ horaSaida.subtract(10, 'minutes').format("HH:mm:ss") +
+	'&nbsp;</span><span class="btn btn-lg btn-warning glyphicon glyphicon-time" title="Hora extra m&iacute;nima"> Extra<br>&nbsp;'
+	+ horaSaida.add(20, 'minutes').format("HH:mm:ss") +
+	'&nbsp;</span></div>'
+);

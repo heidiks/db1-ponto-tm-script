562f330e4222bfb9698cf26385f233c71aa4f7c3
Heidi Kussakawa <heidiks@gmail.com>
2015-05-19 17:55:53 -0300

    Tooltip com horários

--- calculator.js
+++ calculator.js
@@ -17,6 +17,10 @@ var criaMoment = function(hora) {
 	return moment(DIA_FICTICIO + hora);
 };
 
+var montaTooltip = function() {
+	return "Manh&atilde;: " + manhaInicio + " ~ " + manhaFim + "&#013;Tarde: " + tardeInicio;	
+};
+
 var DIA_FICTICIO = "2011-01-01 ";
 var JORNADA_TOTAL_SEGUNDOS = 31680;
 
@@ -38,15 +42,15 @@ if (typeof manhaInicio != 'undefined' && typeof manhaFim != 'undefined' && typeo
 	    link.media = 'all';
 	    head.appendChild(link);
 	}
-
+    
 	$(".tabExterna").parent().prepend(
 		"<div id=\"dvCalculator\" class=\"panel panel-primary\" style=\"width:550px\">" +
 			"<div class=\"panel-heading\">" +
 				"<h3 class=\"panel-title\" id=\"panel-title\">Informa&ccedil;&otilde;es<a class=\"anchorjs-link\" href=\"#panel-title\"><span class=\"anchorjs-icon\"></span></a></h3>" +
 			"</div>" +
 		  	"<div class=\"panel-body\">" +
-  				"<span class=\"btn btn-lg btn-success btn-block glyphicon glyphicon-ok\" title=\"Hora minima de sa&iacute;da\"> Saída:" + horaSaida.subtract(10, 'minutes').format("HH:mm:ss") + "</span>" +
-  				"<span class=\"btn btn-lg btn-default btn-block glyphicon glyphicon-time\" title=\"Hora extra m&iacute;nima\"> Extra:" + horaSaida.add(20, 'minutes').format("HH:mm:ss") + "</span>" +
+  				"<span class=\"btn btn-lg btn-success btn-block glyphicon glyphicon-ok\" title=\"" + montaTooltip() + "\"> Saída:" + horaSaida.subtract(10, 'minutes').format("HH:mm:ss") + "</span>" +
+  				"<span class=\"btn btn-lg btn-default btn-block glyphicon glyphicon-time\" title=\"" + montaTooltip() + "\"> Extra:" + horaSaida.add(20, 'minutes').format("HH:mm:ss") + "</span>" +
  		 	"</div>"+
 		"</div>"
 	);

042eb4c1108c2a0ada3ddb5460fbe67f4bc2983c
Heidi Kussakawa <heidiks@gmail.com>
2015-05-19 08:24:10 -0300

    Refactory

--- calculator.js
+++ calculator.js
@@ -11,11 +11,11 @@ var calculaDiferenca = function(horaA, horaB) {
 	var momentB = criaMoment(horaB);
 
 	return momentA.diff(momentB, 'second', true);
-}
+};
 
 var criaMoment = function(hora) {
 	return moment(DIA_FICTICIO + hora);
-}
+};
 
 var DIA_FICTICIO = "2011-01-01 ";
 var JORNADA_TOTAL_SEGUNDOS = 31680;
@@ -24,24 +24,26 @@ var periodoTrabalhadoManha = calculaDiferenca(manhaFim, manhaInicio);
 
 var horaSaida = criaMoment(tardeInicio).add(JORNADA_TOTAL_SEGUNDOS - periodoTrabalhadoManha, "second");
 
-// gambeta para carregar css
-var cssId = 'myCss';
-if (!document.getElementById(cssId))
-{
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = cssId;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css';
-    link.media = 'all';
-    head.appendChild(link);
-}
+if (typeof manhaInicio != 'undefined' && typeof manhaFim != 'undefined' && typeof tardeInicio != 'undefined') {
+	// gambeta para carregar css
+	var cssId = 'myCss';
+	if (!document.getElementById(cssId))
+	{
+	    var head  = document.getElementsByTagName('head')[0];
+	    var link  = document.createElement('link');
+	    link.id   = cssId;
+	    link.rel  = 'stylesheet';
+	    link.type = 'text/css';
+	    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css';
+	    link.media = 'all';
+	    head.appendChild(link);
+	}
 
-$("body").append (
-	'<div id="dvCalculator"><span class="btn btn-lg btn-primary glyphicon glyphicon-ok" title="Hora minima de  sa&iacute;da"> Sa&iacute;da<br>&nbsp;'
-	+ horaSaida.subtract(10, 'minutes').format("HH:mm:ss") +
-	'&nbsp;</span><span class="btn btn-lg btn-warning glyphicon glyphicon-time" title="Hora extra m&iacute;nima"> Extra<br>&nbsp;'
-	+ horaSaida.add(20, 'minutes').format("HH:mm:ss") +
-	'&nbsp;</span></div>'
-);
+	$("body").append (
+		'<div id="dvCalculator"><span class="btn btn-lg btn-primary glyphicon glyphicon-ok" title="Hora minima de  sa&iacute;da"> Sa&iacute;da<br>&nbsp;'
+		+ horaSaida.subtract(10, 'minutes').format("HH:mm:ss") +
+		'&nbsp;</span><span class="btn btn-lg btn-warning glyphicon glyphicon-time" title="Hora extra m&iacute;nima"> Extra<br>&nbsp;'
+		+ horaSaida.add(20, 'minutes').format("HH:mm:ss") +
+		'&nbsp;</span></div>'
+	);
+}

6f585bd7e65cbdf03b7fc6df057b8704c75a1be7
Heidi <heidiks@gmail.com>
2015-06-14 18:20:49 -0300

    - Tempo de trabalho - Progressbar - Alteração layout de informações

--- db1-calculator.js
+++ db1-calculator.js
@@ -53,28 +53,33 @@ if($(".tabExterna").length) {
             return moment(DIA_FICTICIO + hora);
         };
 
-        var montaTooltip = function() {
-            return "Manh&atilde;: " + manhaInicio + " ~ " + manhaFim + "&#013;Tarde: " + tardeInicio;   
-        };
-
-        var JORNADA_TOTAL_SEGUNDOS = 31680;
+        var JORNADA_MINIMA_TOTAL_SEGUNDOS = 31680;
 
         var periodoTrabalhadoManha = calculaDiferenca(manhaFim, manhaInicio);
 
-        var horaSaida = criaMoment(tardeInicio).add(JORNADA_TOTAL_SEGUNDOS - periodoTrabalhadoManha, "second");
+        var horaSaida = criaMoment(tardeInicio).add(JORNADA_MINIMA_TOTAL_SEGUNDOS - periodoTrabalhadoManha, "second");
 
+        var timeNow = moment().format("HH:mm:ss");
+        var horaAtualTrabalhadas = moment(DIA_FICTICIO).add(periodoTrabalhadoManha + calculaDiferenca(timeNow, tardeInicio), "second");
+        var porcentagem_horaAtualTrabalhadas = (periodoTrabalhadoManha + calculaDiferenca(timeNow, tardeInicio)) * 100 / JORNADA_MINIMA_TOTAL_SEGUNDOS;
+        
         if (typeof manhaInicio != 'undefined' && typeof manhaFim != 'undefined' && typeof tardeInicio != 'undefined' && horaSaida.isValid()) {
             $(".tabExterna").parent().prepend(
-                "<div id=\"dvCalculator\" class=\"panel panel-primary\" style=\"width:100%\">" +
-                    "<div class=\"panel-heading\">" +
-                        "<h3 class=\"panel-title\" id=\"panel-title\">Informa&ccedil;&otilde;es<a class=\"anchorjs-link\" href=\"#panel-title\"><span class=\"anchorjs-icon\"></span></a></h3>" +
-                    "</div>" +
-                    "<div class=\"panel-body\">" +
-                        "<span class=\"btn btn-lg btn-success btn-block glyphicon glyphicon-ok\" title=\"" + montaTooltip() + "\"> Saída:" + horaSaida.subtract(10, 'minutes').format("HH:mm:ss") + "</span>" +
-                        "<span class=\"btn btn-lg btn-default btn-block glyphicon glyphicon-time\" title=\"" + montaTooltip() + "\"> Extra:" + horaSaida.add(20, 'minutes').format("HH:mm:ss") + "</span>" +
-                    "</div>"+
+                "<div align=\"center\">" +
+                    "<div class=\"well well-lg\">" +
+                        "<h2>Voc&ecirc; j&aacute; cumpriu <strong>"+ horaAtualTrabalhadas.format("HH:mm") +"</strong> horas</h2>" +
+                            "<div class=\"progress\">" +
+                                "<div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"45\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: "+ porcentagem_horaAtualTrabalhadas + "%\">" +
+                                    parseInt(porcentagem_horaAtualTrabalhadas) +"%" + 
+                                "</div>" +
+                            "</div>" +
+                        "<h4>Jornada:</h4>\n" +
+                         "<h4><span class=\"label label-success\">M&iacute;nima: " + horaSaida.subtract(10, 'minutes').format("HH:mm") + "</span>\n" +
+                        "<span class=\"label label-primary\">Normal: " + horaSaida.add(10, 'minutes').format("HH:mm") + "</span>\n" + 
+                        "<span class=\"label label-warning\">Extra: " + horaSaida.add(10, 'minutes').format("HH:mm") + "</span> </h4>\n" +
+                    "</div>\n" +
                 "</div>"
             );
         }
     } 
-}
+}   

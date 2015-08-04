9e4c5ac6a2bbfc8c5fe80e867e29ce711cae42be
Heidi Kussakawa <heidi.kussakawa@db1.com.br>
2015-08-04 18:51:12 -0300

    Test performace

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DB1 ponto TM calculator
 // @namespace    http://your.homepage/
-// @version      0.1
+// @version      0.2
 // @description  TODO 
 // @author       Heidi Kussakawa
 // @match        http://ponto.db1.com.br/ponto/*
@@ -10,4 +10,4 @@
 // ==/UserScript==
 
 $.getScript("http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js");
-$.getScript("http://rawgit.com/heidiks/db1-ponto-tm-script/master/db1-calculator.js");
+$.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/master/db1-calculator.js");

2d9fcded699db4058c5815e223a809e637e950a5
Heidi Kussakawa <heidi.kussakawa@db1.com.br>
2015-08-13 16:48:08 -0300

    Release correção calculo sábado

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DB1 ponto calculator
 // @namespace    https://github.com/heidiks/db1-ponto-tm-script
-// @version      0.2.1
+// @version      0.2.2
 // @description  DB1 ponto calculator 
 // @author       Heidi Kussakawa
 // @match        http://ponto.db1.com.br/ponto/*
@@ -10,4 +10,4 @@
 // ==/UserScript==
 
 $.getScript("http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js");
-$.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/404b8039bd594381db3da57340bd603b0246153c/db1-calculator.js");
+$.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/3e8b5813ba641818dac9507b32aca0460f961adc/db1-calculator.js");

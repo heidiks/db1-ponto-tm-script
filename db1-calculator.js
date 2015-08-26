4c15c2c4c8f41e3d4d3504853219086854ec2319
Gabriel.amaral <gabrielamaral713@gmail.com>
2015-08-26 00:57:06 -0300

    Commit inicial

--- db1-calculator.js
+++ db1-calculator.js
@@ -196,21 +196,6 @@ if($(".tabExterna").length) {
         if (typeof pontoHoje.manhaInicio != 'undefined' && typeof pontoHoje.manhaFim != 'undefined' && typeof pontoHoje.tardeInicio != 'undefined' && horaSaida.isValid()) {
             $(".tabExterna").parent().prepend(
                 "<div align=\"center\">" +
-<<<<<<< HEAD
-                    "<button class=\"btn btn-default pull-right glyphicon glyphicon-refresh\" onClick=\"refresh()\"></button>" +
-                    "<div class=\"well well-lg\">" +
-                        "<h2>Voc&ecirc; j&aacute; cumpriu <strong><span id=\"horarioCumprido\">"+ horaAtualTrabalhadas().format("HH:mm") +"</span></strong> horas</h2>" +
-                            "<div class=\"progress\">" +
-                                "<div id=\"progress-bar\" class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"45\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: "+ porcentagem_horaAtualTrabalhadas() + "%\">" +
-                                    parseInt(porcentagem_horaAtualTrabalhadas()) +"%" + 
-                                "</div>" +
-                            "</div>" +
-                        "<h4>Jornada:</h4>\n" +
-                         "<h4><span class=\"label label-success\">M&iacute;nima: " + horaSaida.subtract(10, 'minutes').format("HH:mm:ss") + "</span>\n" +
-                        "<span class=\"label label-primary\">Normal: " + horaSaida.add(10, 'minutes').format("HH:mm:ss") + "</span>\n" +
-                        "<span class=\"label label-warning\">Extra: " + horaSaida.add(10, 'minutes').format("HH:mm:ss") + "</span> </h4>\n" +
-                    "</div>\n" +
-=======
                 "<button class=\"btn btn-default pull-right glyphicon glyphicon-refresh\" onClick=\"refresh()\"></button>" +
                 "<div class=\"well well-lg\">" +
                 "<h2>Voc&ecirc; j&aacute; cumpriu <strong><span id=\"horarioCumprido\">"+ pontoHoje.horaAtualTrabalhadas().format("HH:mm") +"</span></strong> horas</h2>" +
@@ -224,9 +209,42 @@ if($(".tabExterna").length) {
                 "<span class=\"label label-primary\">Normal: " + horaSaida.add(10, 'minutes').format("HH:mm") + "</span>\n" +
                 "<span class=\"label label-warning\">Extra: " + horaSaida.add(10, 'minutes').format("HH:mm") + "</span> </h4>\n" +
                 "</div>\n" +
->>>>>>> upstream/master
                 "</div>"
             );
         }
     }
-}   
+}
+
+// VAI COMEÇAR A BAGAÇEIRA KKKKK
+
+if($(".tr_cont").length) {
+
+    var cardapioVisivel = false;
+
+    appendNewDiv = function (container, idNewDiv) {
+        container.append("<div id=\"" +idNewDiv+ "\"></div>");
+        return $("#"+idNewDiv);
+    };
+
+    //INIT
+    var content = appendNewDiv($(".tr_cont").parent().parent(),"content");
+
+    exibirCardapio = function() {
+        if(!cardapioVisivel) {
+            var divCardapio = appendNewDiv(content,"cardapio");
+            divCardapio.append("<iframe width=\"100%\" height=\"100%\" src=\"http://www.gastronomiacolherdepau.com.br.usrfiles.com/html/36e98f_eb1aa3ea000108896b1ae72b4c069a5e.html\"></iframe>");
+            cardapioVisivel = true;
+        }
+    };
+
+    abrirPedido = function() {
+
+    };
+
+    // criando e adicionado os botoes
+    var btnCardapio = "<button class=\"btn btn-default pull-right glyphicon glyphicon-lst-alt\" onClick=\"exibirCardapio()\">Cardápio do dia</button>";
+    var btnPedido = "<button class=\"btn btn-default pull-right glyphicon glyphicon-clutery\" onClick=\"abrirPedido()\">Fazer Pedido</button>";
+    var divBotoes = appendNewDiv(content, "botoes");
+    divBotoes.append(btnCardapio).append(btnPedido);
+
+}

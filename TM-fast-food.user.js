b27db799ded2bd4a406328315fbe5a32f66243ab
Gabriel.amaral <gabrielamaral713@gmail.com>
2015-09-02 07:33:43 -0300

    criado arquivo .user e atualizado ações dos botões

---
+++ TM-fast-food.user.js
@@ -0,0 +1,18 @@
+/**
+ * Created by gabriel.amaral on 02/09/2015.
+ */
+
+// ==UserScript==
+// @name         DB1 controle marmita
+// @namespace    https://github.com/heidiks/db1-ponto-tm-script
+// @version      0.1
+// @description  DB1 controle marmita
+// @author       Gabriel Amaral
+// @match        https://docs.google.com/forms/d/1rwyNOuUcu4E2xO3gslohyvfQLrVIR_NkTGAmJOyabbk/viewform?embedded=true
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$("#ss-submit").click(function(){
+
+});
--- db1-fast-food.js
+++ db1-fast-food.js
@@ -6,28 +6,27 @@ if($(".tr_cont").length) {
     var cardapioVisivel = false;
 
     appendNewDiv = function (container, idNewDiv) {
-        container.append("<div id=\"" +idNewDiv+ "\"></div>");
+        var newDiv = "<div id=\"" +idNewDiv+ "\"></div>";
+        container.append(newDiv);
         return $("#"+idNewDiv);
     };
 
     //INIT
-    var content = appendNewDiv($(".tr_cont").parent().parent(),"content");
+    var content = appendNewDiv($(".tr_cont").parent().parent().parent(),"content");
 
     exibirCardapio = function() {
         if(!cardapioVisivel) {
             var divCardapio = appendNewDiv(content,"cardapio");
-            divCardapio.append("<iframe width=\"100%\" height=\"100%\" src=\"http://www.gastronomiacolherdepau.com.br.usrfiles.com/html/36e98f_eb1aa3ea000108896b1ae72b4c069a5e.html\"></iframe>");
+            var iframe ="<iframe width=\"100%\" height=\"100%\" src=\"http://www.gastronomiacolherdepau.com.br.usrfiles.com/html/36e98f_eb1aa3ea000108896b1ae72b4c069a5e.html\"></iframe>";
+            divCardapio.append(iframe);
             cardapioVisivel = true;
         }
     };
 
-    abrirPedido = function() {
-
-    };
-
     // criando e adicionado os botoes
-    var btnCardapio = "<button class=\"btn btn-default pull-right glyphicon glyphicon-lst-alt\" onClick=\"exibirCardapio()\">Cardápio do dia</button>";
-    var btnPedido = "<button class=\"btn btn-default pull-right glyphicon glyphicon-clutery\" onClick=\"abrirPedido()\">Fazer Pedido</button>";
+    var btnCardapio = "<button class=\"btn btn-default pull-right btn-info\" onClick=\"exibirCardapio()\">Cardápio do dia</button>";
+    //var btnCardapio = "<a class='btn btn-default pull-right btn-info' href='http://www.gastronomiacolherdepau.com.br.usrfiles.com/html/36e98f_eb1aa3ea000108896b1ae72b4c069a5e.html' target='_blank'>Cardápio do dia</a>";
+    var btnPedido = "<a class='btn btn-default pull-right btn-success' href='https://docs.google.com/forms/d/1rwyNOuUcu4E2xO3gslohyvfQLrVIR_NkTGAmJOyabbk/viewform?embedded=true' target='_blank' >Fazer Pedido</button>";
     var divBotoes = appendNewDiv(content, "botoes");
     divBotoes.append(btnCardapio).append(btnPedido);
 

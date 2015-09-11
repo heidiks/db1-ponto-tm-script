67e376dbab049ac73e8d55959c58f43e2620679c
Heidi <heidiks@gmail.com>
2015-09-11 19:24:31 -0300

    Release correção caculo hora extra

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DB1 ponto calculator
 // @namespace    https://github.com/heidiks/db1-ponto-tm-script
-// @version      0.2.4
+// @version      0.2.5
 // @description  DB1 ponto calculator 
 // @author       Heidi Kussakawa
 // @match        http://ponto.db1.com.br/ponto/*
@@ -10,5 +10,5 @@
 // ==/UserScript==
 
 $.getScript( "http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" ).done(function() {
-    $.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/fbd48512f0c56b4e183fc2a32a581b3e77ab1054/db1-calculator.js");
+    $.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/9a8c6ee93dd13322ce0a797faf33e1dcb9abc847/db1-calculator.js");
 });

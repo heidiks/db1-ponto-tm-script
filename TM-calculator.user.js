1a578275cbe9eb305ce0c3883ed09a3d2ccba94f
Heidi Kussakawa <heidiks@gmail.com>
2015-09-14 18:18:33 -0300

    Release correção calculo horarios

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DB1 ponto calculator
 // @namespace    https://github.com/heidiks/db1-ponto-tm-script
-// @version      0.2.5
+// @version      0.2.6
 // @description  DB1 ponto calculator 
 // @author       Heidi Kussakawa
 // @match        http://ponto.db1.com.br/ponto/*
@@ -10,5 +10,5 @@
 // ==/UserScript==
 
 $.getScript( "http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" ).done(function() {
-    $.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/9a8c6ee93dd13322ce0a797faf33e1dcb9abc847/db1-calculator.js");
+    $.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/d63333446fdb45eb651c9a1324b1c18b8f4b0899/db1-calculator.js");
 });

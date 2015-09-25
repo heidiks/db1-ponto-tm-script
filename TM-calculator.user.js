7e1314d3456507ee9f5ec3278ee4fb791782b0ac
Heidi Kussakawa <heidiks@gmail.com>
2015-09-25 12:04:37 -0300

    Release userL

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DB1 ponto calculator
 // @namespace    https://github.com/heidiks/db1-ponto-tm-script
-// @version      0.2.8
+// @version      0.2.9
 // @description  DB1 ponto calculator 
 // @author       Heidi Kussakawa
 // @match        http://ponto.db1.com.br/ponto/*
@@ -10,5 +10,5 @@
 // ==/UserScript==
 
 $.getScript( "http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" ).done(function() {
-    $.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/946d9f6489c2afc34bf4f818e4f42dffeef4d565/db1-calculator.js");
+    $.getScript("https://cdn.rawgit.com/heidiks/db1-ponto-tm-script/02cf5bf935ebc9a0ddcf3312b6f601d0dd16278a/db1-calculator.js");
 });

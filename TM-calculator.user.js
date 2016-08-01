27e42d835f0cd9a92ff0115697fa9f8ab9554df7
Heidi Kussakawa <heidiks@gmail.com>
2016-08-01 11:44:41 -0300

    Match url

--- TM-calculator.user.js
+++ TM-calculator.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         DB1 ponto calculator
 // @namespace    https://github.com/heidiks/db1-ponto-tm-script
-// @version      0.3.2
+// @version      0.3.3
 // @description  DB1 ponto calculator 
 // @author       Heidi Kussakawa
 // @match        http://ponto.db1.com.br/ponto/*
+// @match        http://ponto2.db1.com.br/ponto/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
